30f4db161a632067fd4dbdd557fb2a7d2d715884
Michelle-567 <michellendirangu2001@gmail.com>
2023-06-21 13:38:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * print-alphabet - Make the alphabet
+ * Return void
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = ;'a'<= '2'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
